332f195e6d3398f3d0923c94a7fcf5e18724ba3a
kanghyew0n <104333249+kanghyew0n@users.noreply.github.com>
2025-04-29 01:16:25 +0900

    refactor: app 설정

---
+++ src/app/App.tsx
@@ -0,0 +1,12 @@
+import PostManagerPage from "@pages/PostsManagerPage"
+import { BrowserRouter as Router } from "react-router-dom"
+
+const App = () => {
+  return (
+    <Router>
+      <PostManagerPage />
+    </Router>
+  )
+}
+
+export default App
