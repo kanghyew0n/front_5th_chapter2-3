6113358621009ce465b0fc27448049e3540bdc20
kanghyew0n <104333249+kanghyew0n@users.noreply.github.com>
2025-05-01 02:10:37 +0900

    refactor: ui 폴더 개선

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,5 @@
 import PostManagerPage from "@pages/PostsManagerPage"
-import Layout from "@shared/ui/Layout"
+import Layout from "@shared/ui/Layout/Layout"
 import { BrowserRouter as Router } from "react-router-dom"
 
 const App = () => {
