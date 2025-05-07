2cdf001f41b60de53a34cbdf47c46d391573d8b0
kanghyew0n <104333249+kanghyew0n@users.noreply.github.com>
2025-05-08 00:15:58 +0900

    chore: tanstack query 설정

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,14 +1,19 @@
 import PostManagerPage from "@pages/PostsManagerPage"
 import Layout from "@shared/ui/Layout/Layout"
 import { BrowserRouter as Router } from "react-router-dom"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 
 const App = () => {
+  const queryClient = new QueryClient()
+
   return (
-    <Router>
-      <Layout>
-        <PostManagerPage />
-      </Layout>
-    </Router>
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <Layout>
+          <PostManagerPage />
+        </Layout>
+      </Router>
+    </QueryClientProvider>
   )
 }
 
